033a4cc82cc602d0408d9333599dd4a95b30b5ae
시혁 박 <exon.indiv@gmail.com>
2020-09-18 13:30:47 +0900

    simple code

--- index.js
+++ index.js
@@ -4,17 +4,11 @@ const client = new Discord.Client();
 const setting = require('./settings.json')
 const prefix = "!";
 
+const makeDes = (nick) => { return `**\`${nick}\`**로 변경이 완료되었습니다.\n\n※봇의 역할 권한이 명령어를 쓰는 사람보다 높지 않으면 닉네임이 바뀌지 않습니다. \n(서버 관리자는 명령어를 쓸 수 없습니다.)`}
+
 
 function info(msg) {
   console.log(`[INFO] ${msg}`);
-}
-
-function mkSuccess(nick){
-  let embed = new Discord.MessageEmbed()
-  .setColor("#42f55a")
-  .setTitle("성공")
-  .setDescription(`**\`${nick}\`**로 변경이 완료되었습니다.\n\n※봇의 역할 권한이 명령어를 쓰는 사람보다 높지 않으면 닉네임이 바뀌지 않습니다. \n(서버 관리자는 명령어를 쓸 수 없습니다.)`);
-  return embed;
 }
 
 
@@ -27,38 +21,15 @@ client.on("ready", () => {
 client.on("message", (msg) => {
   const args = msg.content.split(" ");
   const cmd = msg.content.slice(1, msg.content.length);
-
-  function sendSuccess(embed, nick){
-    msg.channel.send(embed);
-    msg.member.setNickname(nick, "엄준식");
-  }
   
   
   const where = [
-    "롯데리아",
-    "목욕탕",
-    "피시방",
-    "놀이터",
-    "동네",
-    "공터",
-    "초등학교",
-    "중학교",
-    "고등학교",
-    "모텔",
-    "대학교",
-    "백화점",
-    "은행",
-    "호수"
+    "롯데리아", "목욕탕", "피시방", "놀이터", "동네", "공터", "초등학교", "중학교", "고등학교", "모텔", "대학교", "백화점",
+    "은행", "호수"
   ];
 
   const doing = [
-    "화장실문지기",
-    "훔쳐보는",
-    "몰래들어가는",
-    "파괴하는",
-    "관종",
-    "자전거도둑",
-    "신발도둑",
+    "화장실문지기", "훔쳐보는", "몰래들어가는", "파괴하는", "관종", "자전거도둑", "신발도둑"
   ];
 
   if (!msg.content.startsWith(prefix)) return;
@@ -71,35 +42,16 @@ client.on("message", (msg) => {
       );
 
   if (cmd.startsWith("randUm")) {
-
     info(args);
+    let nick = [where[Math.floor(Math.random() * where.length)] + doing[Math.floor(Math.random() * doing.length)] + msg.author.username,
+    where[Math.floor(Math.random() * where.length)] + doing[Math.floor(Math.random() * doing.length)] + args[1], 
+    args[2] + where[Math.floor(Math.random() * where.length)] + doing[Math.floor(Math.random() * doing.length)] + args[1]]
+    [args.length - 1]
 
-    if (args.length === 1) {
-      let nick =
-      where[Math.floor(Math.random() * where.length)] +
-      doing[Math.floor(Math.random() * doing.length)] +
-      msg.author.username;
+    msg.channel.send(new Discord.MessageEmbed({title:'성공', description:makeDes(nick), color:'#42f55a'}))
+    msg.member.setNickname(nick, "엄준식")
+  } else if (cmd === "help") msg.channel.send(helpEmbed);
 
-      sendSuccess(mkSuccess(nick), nick)
-    } else if (args.length === 2) {
-      let nick =
-        where[Math.floor(Math.random() * where.length)] +
-        doing[Math.floor(Math.random() * doing.length)] +
-        args[1];
-
-      sendSuccess(mkSuccess(nick), nick)
-    } else if (args.length === 3) {
-      let nick =
-        args[2] +
-        where[Math.floor(Math.random() * where.length)] +
-        doing[Math.floor(Math.random() * doing.length)] +
-        args[1];
-
-      sendSuccess(mkSuccess(nick), nick)
-    }
-  } else if (cmd === "help") {
-    msg.channel.send(helpEmbed);
-  }
 });
 
 
