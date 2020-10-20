977a3d19b890c89a75cbebc0b0345a09677c5113
시혁 박 <exon.indiv@gmail.com>
2020-10-20 21:35:07 +0900

    Add aje time

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const where = [ "롯데리아", "목욕탕", "피시방", "놀이터", "동네",
 const doing = [ "화장실문지기", "훔쳐보는", "몰래들어가는", "파괴하는", "관종", "자전거도둑", "신발도둑" ]
 const helpEmbed = new MessageEmbed({color:"#42f55a", title:"How To Use", description:"1. !randUm [닉네임] [거주지]\n2. 닉네임을 적지 않으면 디스코드 이름이 들어간다\n3. 거주지를 적지 않으면 거주지가 들어가지 않은 닉네임이 만들어진다\n4. 닉네임에 띄어쓰기를 넣으면 제대로 작동하지 않는다\n5. 엄준식은 존재한다.\n\n[초대링크](https://discord.com/api/oauth2/authorize?client_id=737503637399666711&permissions=134217728&scope=bot)"})
 
-
 client.on("ready", () => {
   console.log(`[System] Start`);
 });
@@ -32,17 +31,12 @@ client.on("message", (msg) => {
     msg.member.setNickname(nick, "엄준식")
   } else if (cmd === "help") msg.channel.send(helpEmbed)
   else if (cmd === 'aje') {
-    axios.request({
-      method: 'GET',
-      url: 'https://aje.teamwv.ml/api/json',
-      headers: { 
-        'Content-Type': 'application/json'
-      }
-    }).then(res => msg.channel.send(new MessageEmbed({title:res.data.que, description: `||${res.data.answer}||`, color:'#42f55a'})))
+    axios.request({ method: 'GET', url: 'https://aje.teamwv.ml/api/json', headers: { 'Content-Type': 'application/json' }})
+    .then(res => {
+      msg.channel.send(new MessageEmbed({title: res.data.que, description: '과연 답은? 두구두구두구', color:'#42f55a'}))
+      setTimeout(() => msg.channel.send(new MessageEmbed({title: res.data.que, description: `||${res.data.answer}||`, color:'#42f55a'})), 3000)
+    })
   }
-
-
-});
-
+})
 
 client.login(token)
