4d60c94d7c3fe82c86f37920716db1cc823847f1
pmh-only <pmhstudio.pmh@gmail.com>
2020-10-22 20:49:09 +0900

    much nicer codes

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,9 @@ module.exports = {
         "es2021": true,
         "node": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "standard"
+    ],
     "parserOptions": {
         "ecmaVersion": 12
     },
